8e3aa3ef9a1562184f6e2d9966659d05f188514a
Jakub Lukasik <lukasik.lukasik@gmail.com>
2017-10-03 00:20:47 +0200

    raw input key processing WIP 3 read input as Unicode WIP created USB HID enum

--- src/core/input/input_win.h
+++ src/core/input/input_win.h
@@ -1,9 +1,27 @@
 #pragma once
 
+/*
+- Pause will send 0xE11D then 0x45, the value I use is 0xE11D45. You'll need to remember reading 0xE11D to be able to differenciate between "Pause" and "Num lock"
+- Print screen will send 0xE02A then 0xE037
+- Insert, Delete, Home, End, Page Up, Page Down, Up, Down, Left, Right: when Num Lock is on or one or both shift keys are pressed, 0xE02A, 0xE0AA, 0xE036, 0xE0B6 can be as postfix or prefix
+- Num pad divide and one or both shift are pressed: 0xE02A, 0xE0AA, 0xE036, 0xE0B6 can be as postfix or prefix
+*/
 
-enum class Scancode
+
+static const unsigned int MAX_MAKECODE = 0xff;
+static const unsigned int SC_E0 = 0xE000; //E0 bits
+static const unsigned int SC_E1 = 0xE100; //E1 bits
+static const unsigned int SC_PAUSE_PART1 = 0xE11D; //The pause scancode consists from 2 parts
+static const unsigned int SC_PAUSE_PART2 = 0x45; //WM_INPUT with 0xE11D and one WM_INPUT with 0x45
+
+static const unsigned int SC_IGNORE1 = 0xE02A; //0xE02A: first part of the Print Screen scancode if no Shift, Control or Alt keys are pressed
+static const unsigned int SC_IGNORE2 = 0xE0AA; //0xE02A, 0xE0AA, 0xE036, 0xE0B6: generated in addition of Insert, Delete, Home, End, Page Up, Page Down, Up, Down, Left, Right when num lock is on; or when num lock is off but one or both shift keys are pressed
+static const unsigned int SC_IGNORE3 = 0xE036; //0xE02A, 0xE0AA, 0xE036, 0xE0B6: generated in addition of Numpad Divide and one or both Shift keys are pressed
+static const unsigned int SC_IGNORE4 = 0xE0B6; //some of those a break scancode, so we ignore them
+
+
+enum Scancode
 {
-
 	sc_escape =				0x01,
 	sc_1 =					0x02,
 	sc_2 =					0x03,
@@ -177,3 +195,253 @@ enum class Scancode
 	- when pressed at the same time as one or both control keys, generates a 0xE046 (sc_cancel) and the string for that scancode is "break".
 	*/
 };
+
+
+enum class Scancode_USB_HID
+{
+	SystemPower =				0x81, //0xE05E
+	SystemSleep =				0x82, //0xE05F
+	SystemWake =				0x83, //0xE063
+
+	None =						0x00, //None
+	Overrun_Error =				0x01, //FF
+	POST_Fail =					0x02, //FC
+	ErrorUndefined =			0x03, //NA
+	a =							0x04, //1E
+	b =							0x05, //30
+	c =							0x06, //2E
+	d =							0x07, //20
+	e =							0x08, //12
+	f =							0x09, //21
+	g =							0x0A, //22
+	h =							0x0B, //23
+	i =							0x0C, //17
+	j =							0x0D, //24
+	k =							0x0E, //25
+	l =							0x0F, //26
+	m =							0x10, //32
+	n =							0x11, //31
+	o =							0x12, //18
+	p =							0x13, //19
+	q =							0x14, //10
+	r =							0x15, //13
+	s =							0x16, //1F
+	t =							0x17, //14
+	u =							0x18, //16
+	v =							0x19, //2F
+	w =							0x1A, //11
+	x =							0x1B, //2D
+	y =							0x1C, //15
+	z =							0x1D, //2C
+	n1 =						0x1E, //02
+	n2 =						0x1F, //03
+	n3 =						0x20, //04
+	n4 =						0x21, //05
+	n5 =						0x22, //06
+	n6 =						0x23, //07
+	n7 =						0x24, //08
+	n8 =						0x25, //09
+	n9 =						0x26, //0A
+	n0 =						0x27,//0B
+	Return =					0x28, //1C
+	Escape =					0x29, //01
+	Backspace =					0x2A, //0E
+	Tab =						0x2B, //0F
+	Space =						0x2C, //39
+	Minus =						0x2D, //0C
+	Equals =					0x2E, //0D
+	BracketLeft =				0x2F, //1A
+	BracketRight =				0x30, //1B
+	Backslash =					0x31, //2B
+	Europe1 =					0x32, //WTF?
+	Semicolon =					0x33, //27
+	Apostrophe =				0x34, //28
+	Grave =						0x35, //29
+	Comma =						0x36, //33
+	Period =					0x37, //34
+	Slash =						0x38, //35
+	CapsLock =					0x39, //3A
+	F1 =						0x3A, //3B
+	F2 =						0x3B, //3C
+	F3 =						0x3C, //3D
+	F4 =						0x3D, //3E
+	F5 =						0x3E, //3F
+	F6 =						0x3F, //40
+	F7 =						0x40, //41
+	F8 =						0x41, //42
+	F9 =						0x42, //43
+	F10 =						0x43, //44
+	F11 =						0x44, //57
+	F12 =						0x45, //58
+	PrintScreen =				0x46, //E037
+	ScrollLock =				0x47, //46
+	Break =						0x48, //E046
+	Pause =						0x48, //E11D45
+	Insert =					0x49, //E052
+	Home =						0x4A, //E047
+	PageUp =					0x4B, //E049
+	Delete =					0x4C, //E053
+	End =						0x4D, //E04F
+	PageDown =					0x4E, //E051
+	RightArrow =				0x4F, //E04D
+	LeftArrow =					0x50, //E04B
+	DownArrow =					0x51, //E050
+	UpArrow =					0x52, //E048
+	NumLock =					0x53, //45
+	NumpadSlash =				0x54, //E035
+	NumpadMultiply =			0x55, //37
+	NumpadMinus =				0x56, //4A
+	NumpadPlus =				0x57, //4E
+	NumpadEnter =				0x58, //E01C
+	Numpad1 =					0x59, //4F
+	Numpad2 =					0x5A, //50
+	Numpad3 =					0x5B, //51
+	Numpad4 =					0x5C, //4B
+	Numpad5 =					0x5D, //4C
+	Numpad6 =					0x5E, //4D
+	Numpad7 =					0x5F, //47
+	Numpad8 =					0x60, //48
+	Numpad9 =					0x61, //49
+	Numpad0 =					0x62, //52
+	NumpadPeriod =				0x63, //53
+	Europe2 =					0x64, //56
+	App =						0x65, //E05D
+	KeyboardPower =				0x66, //E05E
+	NumpadResolution =			0x67, //59
+	F13 =						0x68, //64
+	F14 =						0x69, //65
+	F15 =						0x6A, //66
+	F16 =						0x6B, //67
+	F17 =						0x6C, //68
+	F18 =						0x6D, //69
+	F19 =						0x6E, //6A
+	F20 =						0x6F, //6B
+	F21 =						0x70, //6C
+	F22 =						0x71, //6D
+	F23 =						0x72, //6E
+	F24 =						0x73, //76
+	KeyboardExecute =			0x74, //UNASSIGNED
+	KeyboardHelp =				0x75, //UNASSIGNED
+	KeyboardMenu =				0x76, //UNASSIGNED
+	KeyboardSelect =			0x77, //UNASSIGNED
+	KeyboardStop =				0x78, //UNASSIGNED
+	KeyboardAgain =				0x79, //UNASSIGNED
+	KeyboardUndo =				0x7A, //UNASSIGNED
+	KeyboardCut =				0x7B, //UNASSIGNED
+	KeyboardCopy =				0x7C, //UNASSIGNED
+	KeyboardPaste =				0x7D, //UNASSIGNED
+	KeyboardFind =				0x7E, //UNASSIGNED
+	KeyboardMute =				0x7F, //UNASSIGNED
+	KeyboardVolumeUp =			0x80, //UNASSIGNED
+	KeyboardVolumeDn =			0x81, //UNASSIGNED
+	KeyboardLockingCapsLock =	0x82, //UNASSIGNED
+	KeyboardLockingNumLock =	0x83, //UNASSIGNED
+	KeyboardLockingScrollLock = 0x84, //UNASSIGNED
+	NumPadComa =				0x85, //7E
+	NumLockEquals =				0x86, //UNASSIGNED
+	KeyboardIntl1 =				0x87, //73
+	KeyboardIntl2 =				0x88, //70
+	KeyboardIntl2 =				0x89, //7D
+	KeyboardIntl4 =				0x8A, //79
+	KeyboardIntl5 =				0x8B, //7B
+	KeyboardIntl6 =				0x8C, //5C
+	KeyboardIntl7 =				0x8D, //UNASSIGNED
+	KeyboardIntl8 =				0x8E, //UNASSIGNED
+	KeyboardIntl9 =				0x8F, //UNASSIGNED
+	KeyboardLang1 =				0x90, //F2
+	KeyboardLang2 =				0x91, //F1
+	KeyboardLang3 =				0x92, //78
+	KeyboardLang4 =				0x93, //77
+	KeyboardLang5 =				0x94, //76
+	KeyboardLang6 =				0x95, //UNASSIGNED
+	KeyboardLang7 =				0x96, //UNASSIGNED
+	KeyboardLang8 =				0x97, //UNASSIGNED
+	KeyboardLang9 =				0x98, //UNASSIGNED
+	KeyboardAlternateErase =	0x99, //UNASSIGNED
+	KeyboardSysReq_Attention =	0x9A, //UNASSIGNED
+	KeyboardCancel =			0x9B, //UNASSIGNED
+	KeyboardClear =				0x9C, //UNASSIGNED
+	KeyboardPrior =				0x9D, //UNASSIGNED
+	KeyboardReturn =			0x9E, //UNASSIGNED
+	KeyboardSeparator =			0x9F, //UNASSIGNED
+	KeyboardOut =				0xA0, //UNASSIGNED
+	KeyboardOper =				0xA1, //UNASSIGNED
+	KeyboardClear_Again =		0xA2, //UNASSIGNED
+	KeyboardCrSel_Props =		0xA3, //UNASSIGNED
+	KeyboardExSel =				0xA4, //UNASSIGNED
+	//RESERVED A5 - DF //RESERVED
+	LeftControl =				0xE0, //1D
+	LeftShift =					0xE1, //2A
+	LeftAlt =					0xE2, //38
+	LeftGUI =					0xE3, //E05B
+	RightControl =				0xE4, //E01D
+	RightShift =				0xE5, //36
+	RightAlt =					0xE6, //E038
+	RightGUI =					0xE7, //E05C
+	//RESERVED = 0xE8 - FFFF RESERVED RESERVED RESERVED RESERVED
+	
+	ScanNextTrack =				0x00B5, //E019
+	ScanPreviousTrack =			0x00B6, //E010
+	Stop =						0x00B7, //E024
+	Play_Pause =				0x00CD, //E022
+	Mute =						0x00E2, //E020
+	BassBoost =					0x00E5, //UNASSIGNED
+	Loudness =					0x00E7, //UNASSIGNED
+	VolumeUp =					0x00E9, //E030
+	VolumeDown =				0x00EA, //E02E
+	BassUp =					0x0152, //UNASSIGNED
+	BassDown =					0x0153, //UNASSIGNED
+	TrebleUp =					0x0154, //UNASSIGNED
+	TrebleDown =				0x0155, //UNASSIGNED
+	MediaSelect =				0x0183, //E06D
+	Mail =						0x018A, //E06C
+	Calculator =				0x0192, //E021
+	MyComputer =				0x0194, //E06B
+	WWWSearch =					0x0221, //E065
+	WWWHome =					0x0223, //E032
+	WWWBack =					0x0224, //E06A
+	WWWForward =				0x0225, //E069
+	WWWStop =					0x0226, //E068
+	WWWRefresh =				0x0227, //E067
+	WWWFavorites =				0x022A, //E066
+};
+
+
+/*
+- bit 16 - 23 contains the first byte of the scancode
+- bit 24 indicates that the scancode is 2 bytes (extended)
+*/
+inline unsigned int getScancodeName(unsigned int scancode, char* buffer, unsigned int bufferLength)
+{
+
+	unsigned int result = 0;
+	unsigned int extended = scancode & 0xffff00;
+	unsigned int lParam = 0;
+
+	if(extended != 0)
+	{
+		if(extended == 0xE11D00)
+			lParam = 0x45 << 16;
+		else
+			lParam = (0x100 | (scancode & 0xff)) << 16;
+	}
+	else
+	{
+		lParam = scancode << 16;
+
+		if(scancode == 0x45)
+			lParam |= (0x1 << 24);
+	}
+
+	result = GetKeyNameText(lParam, buffer, bufferLength);
+	return result;
+}
+
+/*
+potrebujem:
+handle WM_KEYDOWN, WM_KEYUP pre VK ?
+handle WM_INPUT pre raw input
+scancode -> USB HID
+USB HID -> key name
+
+*/
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -137,9 +137,7 @@ private:
 
 	void HandleRawInput(LPARAM lParam)
 	{
-		static bool pauseScancodeRead = false;
-
-		static RAWINPUT raw;
+		RAWINPUT raw;
 		UINT size = sizeof(RAWINPUT);
 
 		if(GetRawInputData((HRAWINPUT)lParam, RID_INPUT, &raw, &size, sizeof(RAWINPUTHEADER)) == (UINT)-1)
@@ -148,59 +146,114 @@ private:
 			return;
 		}
 
-		//LogInfo("RawInput: dwType: %d, dwSize: %d, hDevice: %d, wParam: %d\n", raw.header.dwType, raw.header.dwSize, raw.header.hDevice, raw.header.wParam);
 		if(raw.header.dwType == RIM_TYPEKEYBOARD)
 		{
-			unsigned int pressed = 0;
-			unsigned int scancode = raw.data.keyboard.MakeCode; /* MakeCode is unsigned short. */
+			static bool pauseScancodeRead = false;
+			bool pressed = false;
+			unsigned int scancode = raw.data.keyboard.MakeCode; // MakeCode is USHORT
 			unsigned short flags = raw.data.keyboard.Flags;
-			ASSERT(scancode <= 0xff);
+			ASSERT(scancode <= MAX_MAKECODE);
 
 			if((flags & RI_KEY_BREAK) == 0)
-				pressed = 1;
+				pressed = true;
 
 			if(flags & RI_KEY_E0)
-				scancode |= 0xE000;
+				scancode |= SC_E0;
 			else if(flags & RI_KEY_E1)
-				scancode |= 0xE100;
+				scancode |= SC_E1;
 
-			/* The pause scancode is in 2 parts: a WM_INPUT with 0xE11D and one WM_INPUT with 0x45. */
-			if(scancode == 0xE11D)
+			if(scancode == SC_PAUSE_PART1) //The pause scancode is in 2 parts: a WM_INPUT with 0xE11D and one WM_INPUT with 0x45
 			{
 				pauseScancodeRead = true;
 			}
 			else if(pauseScancodeRead)
 			{
-				if(scancode == 0x45)
-					scancode = (unsigned int)Scancode::sc_pause;
+				if(scancode == SC_PAUSE_PART2)
+					scancode = sc_pause;
 				pauseScancodeRead = false;
 			}
-			else if(scancode == 0x54)
+			else if(scancode == sc_alt_printScreen) //Alt + print screen return scancode 0x54 but we want it to return 0xE037 because 0x54 will not return a name for the key
 			{
-				/* Alt + print screen return scancode 0x54 but we want it to return 0xE037 because 0x54 will not return a name for the key. */
-				scancode = (unsigned int)Scancode::sc_printScreen;
+				scancode = sc_printScreen;
 			}
 			
-			/*
-			Some scancodes we can ignore:
-			- 0xE11D: first part of the Pause scancode (handled above);
-			- 0xE02A: first part of the Print Screen scancode if no Shift, Control or Alt keys are pressed;
-			- 0xE02A, 0xE0AA, 0xE036, 0xE0B6: generated in addition of Insert, Delete, Home, End, Page Up, Page Down, Up, Down, Left, Right when num lock is on; or when num lock is off but one or both shift keys are pressed;
-			- 0xE02A, 0xE0AA, 0xE036, 0xE0B6: generated in addition of Numpad Divide and one or both Shift keys are pressed;
-			- Some of those a break scancode;
-
-			When holding a key down, the pre/postfix (0xE02A) is not repeated.
-			*/
-
-			if(scancode == 0xE11D || scancode == 0xE02A || scancode == 0xE0AA || scancode == 0xE0B6 || scancode == 0xE036)
+			//some of those a break scancode, so we ignore them
+			if(scancode == SC_PAUSE_PART1 || scancode == SC_IGNORE1 || scancode == SC_IGNORE2 || scancode == SC_IGNORE3 || scancode == SC_IGNORE4)
 				return;
 
 			// getting a human-readable string
-			UINT key = (scancode << 16);
-			char buffer[512] = {};
-			GetKeyNameText((LONG)key, buffer, 512);treba prerobit
+			//char buffer[512] = {};
+			//getScancodeName(scancode, buffer, 512);
 
-			LogInfo("%s\n", buffer);
+			//LogInfo("%s : %s\n", buffer, (pressed) ? "down" : "up");
+
+
+			//------------------------------------------------------------------------------------
+			// GET UTF CHAR
+			static unsigned char keyState[256] = {};
+
+			USHORT VKey = raw.data.keyboard.VKey;
+			bool e0 = (flags & RI_KEY_E0) != 0;
+			bool e1 = (flags & RI_KEY_E1) != 0;
+
+			// these are unasigned but not reserved as of now.
+			// this is bad but, you know, we'll fix it if it ever breaks.
+			#define VK_LRETURN         0x9E
+			#define VK_RRETURN         0x9F
+
+			#define UPDATE_KEY_STATE(key) do { keyState[key] = (flags & 1) ? 0 : 0xff; } while(0)
+			// note: we set all bits in the byte if the key is down. 
+			// This is becasue windows expects it to be in the high_order_bit (when using it for converting to unicode for example)
+			// and I like it to be in the low_order_bit,  
+			if(VKey == VK_CONTROL)
+			{
+				if(e0)	UPDATE_KEY_STATE(VK_RCONTROL);
+				else	UPDATE_KEY_STATE(VK_LCONTROL);
+				keyState[VK_CONTROL] = keyState[VK_RCONTROL] | keyState[VK_LCONTROL];
+			}
+			else if(VKey == VK_SHIFT)
+			{
+				// because why should any api be consistant lol
+				// (because we get different scancodes for l/r-shift but not for l/r ctrl etc... but still)
+				UPDATE_KEY_STATE(MapVirtualKey(raw.data.keyboard.MakeCode, MAPVK_VSC_TO_VK_EX));
+				keyState[VK_SHIFT] = keyState[VK_LSHIFT] | keyState[VK_RSHIFT];
+			}
+			else if(VKey == VK_MENU)
+			{
+				if(e0)	UPDATE_KEY_STATE(VK_LMENU);
+				else	UPDATE_KEY_STATE(VK_RMENU);
+				keyState[VK_MENU] = keyState[VK_RMENU] | keyState[VK_LMENU];
+			}
+			else if(VKey == VK_RETURN)
+			{
+				if(e0) UPDATE_KEY_STATE(VK_RRETURN);
+				else	UPDATE_KEY_STATE(VK_LRETURN);
+				keyState[VK_RETURN] = keyState[VK_RRETURN] | keyState[VK_LRETURN];
+			}
+			else
+			{
+				UPDATE_KEY_STATE(VKey);
+			}
+			#undef UPDATE_KEY_STATE
+
+			if(pressed)
+			{
+				//char utf8_buffer[32];
+				//char *buff = 0;
+				//int utf8_len = 0;
+
+				// get unicode.
+				wchar_t utf16_buffer[16] = { 0 };
+				//simulating altgr, assumes all leftalts is algr
+				// which seem to work since ctrl is ignored on US versions of ToUnicode. Bad way of doing it, Not sure how to detect if left alt is altgr though.
+				unsigned char ctrl = keyState[VK_CONTROL];
+				keyState[VK_CONTROL] |= keyState[VK_RMENU];
+				int utf16_len = ToUnicode(VKey, raw.data.keyboard.MakeCode, keyState, utf16_buffer, sizeof(utf16_buffer) / sizeof(utf16_buffer[0]), 0);
+				keyState[VK_CONTROL] = ctrl;
+
+				if(utf16_len > 0)
+					OutputDebugStringW(utf16_buffer);
+			}
 		}
 	}
 
@@ -208,13 +261,29 @@ private:
 	{
 		switch(msg)
 		{
-			//case WM_KILLFOCUS: m_engine->getInputSystem().enable(false); break;
-			//case WM_SETFOCUS: m_engine->getInputSystem().enable(true); break;
-			case WM_CLOSE: PostQuitMessage(0); break;
+			/*case WM_KILLFOCUS:
+				m_engine->getInputSystem().enable(false);
+				return 0;
+				break;*/
+			/*case WM_SETFOCUS:
+				m_engine->getInputSystem().enable(true);
+				return 0;
+				break;*/
+			case WM_CLOSE:
+				PostQuitMessage(0);
+				break;
 			//case WM_MOVE:
 			//case WM_SIZE: onResize(); break;
-			case WM_QUIT: m_finished = true; break;
-			case WM_INPUT: HandleRawInput(lparam); break;
+			case WM_QUIT:
+				m_finished = true;
+				break;
+			case WM_INPUT:
+				HandleRawInput(lparam);
+				break;
+			case WM_SYSCOMMAND:
+				if(wparam == SC_KEYMENU) //Remove beeping sound when ALT + some key is pressed.
+					return 0;
+				break;
 		}
 		return DefWindowProc(hwnd, msg, wparam, lparam);
 	}
@@ -222,8 +291,6 @@ private:
 	static LRESULT CALLBACK MsgProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	{
 		ASSERT(s_instance != nullptr);
-		//if(!s_instance)
-			//return DefWindowProc(hwnd, msg, wparam, lparam);
 
 		return s_instance->OnMessage(hwnd, msg, wparam, lparam);
 	}
